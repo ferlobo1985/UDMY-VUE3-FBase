4ffb016b7fe604d92b2d1bfcefdb2af398d5726a
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-27 16:57:53 -0300

    04-Posting Docs
    04-Posting Docs

--- src/components/admin/add_article.vue
+++ src/components/admin/add_article.vue
@@ -21,13 +21,23 @@
 </template>
 
 <script setup>
-  import { reactive } from 'vue';
-  const formData = reactive({
-    title:'',
-    description:''
-  });
+    import { DB } from '@/firebase/config';
+    import { collection, addDoc } from 'firebase/firestore'
+    import { reactive } from 'vue';
 
-  const submitForm = () => {
-    console.log(formData);
-  }
+    const formData = reactive({
+        title:'',
+        description:''
+    });
+
+    const submitForm = async() => {
+        const notesCollection = collection(DB,'notes');
+
+        await addDoc(notesCollection,{
+            ...formData
+        });
+
+        formData.title = '';
+        formData.description = '';
+    }
 </script>
