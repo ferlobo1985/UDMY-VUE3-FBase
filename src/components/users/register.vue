2e685409a4433b0bbef60322bb8cdfe7470fce90
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-28 16:00:40 -0300

    10-Sign in user
    10-Sign in user

--- src/components/users/register.vue
+++ src/components/users/register.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row justify-content-md-center">
         <div class="col col-lg-5">
-            <h1>Register</h1>
+            <h1 v-text="register ? 'Register':'Sign in'"></h1>
             <hr/>
             <form @submit.prevent="submitForm">
                 <div class="mb-3">
@@ -17,6 +17,12 @@
                     Submit
                 </button>
             </form>
+            <hr/>
+            <button
+                class="btn btn-outline-primary"
+                v-text="register ? 'Want to sign in ?':'Want to register ?'"
+                @click="register = !register"
+            />
         </div>
     </div>
 </template>
@@ -24,7 +30,10 @@
 
 <script setup>
     import { AUTH } from '@/firebase/config';
-    import { createUserWithEmailAndPassword } from 'firebase/auth'
+    import { 
+        createUserWithEmailAndPassword,
+        signInWithEmailAndPassword
+    } from 'firebase/auth'
     import { reactive, ref } from 'vue';
     import { useRouter } from 'vue-router';
 
@@ -41,8 +50,23 @@
             registerUser();
         } else {
             // sign in
+            signInUser()
         }
 
+    }
+
+    const signInUser = async() => {
+        try{
+            const response = await signInWithEmailAndPassword(
+                AUTH,formData.email,formData.password
+            );
+            if(!response){
+                throw new Error('Sorry, something happened');
+            }
+            router.push('/')
+        } catch(error){
+            console.log(error.message)
+        }
     }
 
     const registerUser = async() => {
