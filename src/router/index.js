be364aabd5645a64748226de85817aad75dd693e
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-28 17:04:02 -0300

    13-Route Guards
    13-Route Guards

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,37 @@ import Register from '../components/users/register.vue'
 import AddArticle from '../components/admin/add_article.vue'
 import UpdateArticle from '@/components/admin/update_article.vue'
 
+// firebase
+import { AUTH } from '@/firebase/config'
+
+const isAuth = (to,from,next) =>{
+  let user = AUTH.currentUser;
+  if(!user){
+    next({path:'/users/register'});
+    return;
+  }
+  next();
+  return;
+} 
+
+const isLoggedIn = (to,from,next) => {
+  let user = AUTH.currentUser;
+  if(user){
+    next({path:'/'});
+    return;
+  }
+  next();
+  return;
+}
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
-    { path: '/users/register', component: Register },
-    { path: '/admin/add_article', component: AddArticle },
-    { path: '/admin/update_article/:id', component:UpdateArticle}
+    { path: '/users/register', component: Register, beforeEnter:isLoggedIn },
+    { path: '/admin/add_article', component: AddArticle, beforeEnter: isAuth },
+    { path: '/admin/update_article/:id', component:UpdateArticle, beforeEnter: isAuth}
   ]
 })
 
